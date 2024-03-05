f30e674dcc71048ebf32a9690830cb85f0d96098
Muzammil Hussain Shah <mynameismuzammilhussainshah@gmail.com>
2024-03-06 02:20:11 +0500

    Footer section [UPFATE]

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
               title="Our phone"
               className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
             >
-              +92 342 55 00 944
+              +92 346 41 00 928
             </a>
           </div>
           <div className="flex justify-center items-center">
@@ -20,7 +20,7 @@ const Footer = () => {
               title="Our email"
               className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
             >
-              mnmalikdev@gmail.com
+             mynameismuzammilhussainshah@gmail.com
             </a>
           </div>
           <div className="flex justify-center items-center">
@@ -32,14 +32,14 @@ const Footer = () => {
               title="Our address"
               className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
             >
-              Islamabad,Pakistan
+              Karachi, Pakistan
             </a>
           </div>
         </div>
         <div className="flex flex-col ">
           <div className="flex flex-row items-center mt-1 space-x-3">
             <a
-              href="https://www.linkedin.com/in/nabeelmalik97/"
+              href="https://www.linkedin.com/in/myname-muzammil-shah/"
               className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
             >
               <svg
@@ -53,7 +53,7 @@ const Footer = () => {
                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
               </svg>
             </a>
-            <a href="mailto:mnmalikdev@gmail.com" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
+            <a href="mailto:mynameismuzammilhussainshah@gmail.com" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
               <svg
                 width="30"
                 height="30"
@@ -65,7 +65,7 @@ const Footer = () => {
                 <path d="M19 24h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5h14c2.762 0 5 2.239 5 5v14c0 2.761-2.238 5-5 5zm-.141-6.333c.63 0 1.141-.512 1.141-1.142v-9.05c0-.63-.511-1.142-1.141-1.142h-13.718c-.63 0-1.141.512-1.141 1.142v9.05c0 .63.511 1.142 1.141 1.142h13.718zm-6.859-4.058l-6.228-4.321-.014 7.712h12.457v-7.712l-6.215 4.321zm5.913-6.609c-1.745 1.215-5.913 4.153-5.913 4.153l-5.947-4.153h11.86z" />
               </svg>
             </a>
-            <a href="https://wa.me/923425500944" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
+            <a href="https://wa.me/923464100928" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="30"
@@ -77,7 +77,7 @@ const Footer = () => {
                 <path d="M12.036 5.339c-3.635 0-6.591 2.956-6.593 6.589-.001 1.483.434 2.594 1.164 3.756l-.666 2.432 2.494-.654c1.117.663 2.184 1.061 3.595 1.061 3.632 0 6.591-2.956 6.592-6.59.003-3.641-2.942-6.593-6.586-6.594zm3.876 9.423c-.165.463-.957.885-1.337.942-.341.051-.773.072-1.248-.078-.288-.091-.657-.213-1.129-.417-1.987-.858-3.285-2.859-3.384-2.991-.099-.132-.809-1.074-.809-2.049 0-.975.512-1.454.693-1.653.182-.2.396-.25.528-.25l.38.007c.122.006.285-.046.446.34.165.397.561 1.372.611 1.471.049.099.083.215.016.347-.066.132-.099.215-.198.33l-.297.347c-.099.099-.202.206-.087.404.116.198.513.847 1.102 1.372.757.675 1.395.884 1.593.983.198.099.314.083.429-.05.116-.132.495-.578.627-.777s.264-.165.446-.099 1.156.545 1.354.645c.198.099.33.149.38.231.049.085.049.482-.116.945zm3.088-14.762h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-6.967 19.862c-1.327 0-2.634-.333-3.792-.965l-4.203 1.103 1.125-4.108c-.694-1.202-1.059-2.566-1.058-3.964.002-4.372 3.558-7.928 7.928-7.928 2.121.001 4.112.827 5.609 2.325s2.321 3.491 2.32 5.609c-.002 4.372-3.559 7.928-7.929 7.928z" />
               </svg>
             </a>
-            <a href="https://github.com/mnmalikdev" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
+            <a href="https://github.com/muzammilhussainshah" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="30"
@@ -93,7 +93,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center pt-5 pb-10 border-t gap-4 lg:flex-row">
-        <p className="text-sm text-gray-600 font-bold inline-block ">© Copyright 2022 </p> <p>mnmalikdev </p> <p>All rights reserved.</p>
+        <p className="text-sm text-gray-600 font-bold inline-block ">© Copyright 2024 </p> <p>mynameismuzammilhussainshah@gmail.com </p> <p>All rights reserved.</p>
       </div>
     </div>
   );
